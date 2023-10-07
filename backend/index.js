d1a79f7a2c34f8f5a66941b1f1255ea128f693ff
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-10-07 18:13:11 +0100

    CODE: Connected MongoDB, Created User Model

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//middlewares
+app.use(express.json());
+// app.use(express.static(''))
+
+app.get('/api/test', (req, res) => {
+    res.json('Get test started')
+})
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, async () => {
+    try {
+        try {
+           await mongoose.connect(process.env.MONGO_URI);
+            console.log('Connected to MongoDB...')
+        } catch (error) {
+            console.log(error)
+        }
+        console.log(`API Server started on port ${port}`)
+    } catch (error) {
+        if (error) throw error;
+    }
+    
+})
