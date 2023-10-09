75801c783979ecb09c41e37b722455c9c26ebb63
Francis Ihejirika <francis.ihejirika@gmail.com>
2023-10-09 01:45:43 +0100

    All Default Imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,39 @@
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { UserContextProvider } from "./contexts/UserContext";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import PlacesPage from "./pages/PlacesPage";
+import PlacesFormPage from "./pages/PlacesFormPage";
+import PlacePage from "./pages/PlacePage";
+import BookingsPage from "./pages/BookingsPage";
+import BookingPage from "./pages/BookingPage";
+
 
 function App() {
 
   return (
     <>
-      <div className="text-black text-2xl">Test tailwind</div>
+      <UserContextProvider>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<IndexPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/account" element={<ProfilePage />} />
+            <Route path="/account/places" element={<PlacesPage />} />
+            <Route path="/account/places/new" element={<PlacesFormPage />} />
+            <Route path="/account/places/:id" element={<PlacesFormPage />} />
+            <Route path="/place/:id" element={<PlacePage />} />
+            <Route path="/account/bookings" element={<BookingsPage />} />
+            <Route path="/account/bookings/:id" element={<BookingPage />} />
+          </Route>
+        </Routes>
+      </UserContextProvider>
     </>
   )
 }
